d7fce9168ddc2ec79123b740315ab2c6504b1fbc
chrisamico <eyeseast@gmail.com>
2014-03-25 14:48:08 -0400

    Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Learning Lab
+============
+
+Learning Lab covers experiments in education reform. This is a WordPress theme built using the [Largo] parent theme and [Timber] framework.
+
+ [Largo]: http://largoproject.org/
+ [Timber]: http://jarednova.github.io/timber/
--- functions.php
+++ functions.php
@@ -11,26 +11,23 @@ add_action('wp_enqueue_scripts', 'load_scripts');
 
 define('THEME_URL', get_template_directory_uri());
 
-function add_to_context($data){
+
+function add_to_context($data) {
     /* this is where you can add your own data to Timber's context object */
-    $data['qux'] = 'I am a value set in your functions.php file';
     $data['menu'] = new TimberMenu();
     return $data;
 }
 
-function add_to_twig($twig){
+
+function add_to_twig($twig) {
     /* this is where you can add your own fuctions to twig */
     $twig->addExtension(new Twig_Extension_StringLoader());
 
     return $twig;
 }
 
-function myfoo($text){
-    $text .= ' bar!';
-    return $text;
-}
 
-function load_scripts(){
+function load_scripts() {
     wp_enqueue_script('jquery');
 }
 
