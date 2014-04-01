37908eb18ecc71ff1008c2d406841fa51ff5a21d
chrisamico <eyeseast@gmail.com>
2014-04-01 12:34:33 -0400

    Use comments_template function to enable disqus

--- single.php
+++ single.php
@@ -13,6 +13,6 @@ $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 $context['wp_title'] .= ' - ' . $post->title();
-$context['comment_form'] = TimberHelper::get_comment_form();
+//$context['comment_form'] = TimberHelper::get_comment_form();
 
 Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
