b144cef4e158824e4020f762837d88478c7f7f5e
chrisamico <eyeseast@gmail.com>
2014-04-01 11:24:24 -0400

    Search template and little fixes

--- search.php
+++ search.php
@@ -13,6 +13,7 @@
 $templates = array('search.twig', 'archive.twig', 'index.twig');
 $context = Timber::get_context();
 
+$context['search_query'] = get_search_query();
 $context['title'] = 'Search results for '. get_search_query();
 $context['posts'] = Timber::get_posts();
 
